fc8cd1e3a6db4300a8d43745273f14322fe1b6af
DipoJaiye <durojaiyep@yahoo.co.uk>
2023-06-15 20:22:06 -0700

    add c file to print using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * prints a string with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: 0 (Always success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+
+	return (0);
+}
