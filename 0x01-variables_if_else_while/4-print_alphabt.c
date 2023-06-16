4f4cbdfd51fe8be772a8766c8e67184994fc86fc
DipoJaiye <durojaiyep@yahoo.co.uk>
2023-06-16 00:56:37 -0700

    print lowercase letters except e and q using if conditional

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+	int arrLen;
+	int i;
+
+	arrLen = strlen(alphabets);
+	for (i = 0; i < arrLen; i++)
+	{
+		if (alphabets[i] != 'e' && alphabets[i] != 'q')
+		{
+			putchar(alphabets[i]);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
