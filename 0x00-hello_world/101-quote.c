35473e6aa67b95edd0c3b303b2e12b50f2219a39
DipoJaiye <durojaiyep@yahoo.co.uk>
2023-06-15 21:53:16 -0700

    write to file stream stderr with fwrite

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return: 1 (always fail)
+ */
+int main(void)
+{
+	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	fwrite(message, sizeof(char), strlen(message), stderr);
+
+	return (1);
+}
