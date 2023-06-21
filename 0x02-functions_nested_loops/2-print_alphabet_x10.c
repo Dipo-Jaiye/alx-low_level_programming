aed215566e5e7eec02731b54f818264a6f15eae3
DipoJaiye <durojaiyep@yahoo.co.uk>
2023-06-20 20:48:02 -0700

    modify task to not use standard libraries

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * print_alphabet_x10 - prints all lowercase alphabets
@@ -10,19 +9,21 @@
 void print_alphabet_x10(void)
 {
 	int j;
+	char a;
+	int arrLen;
+
+	a = 'a';
+	arrLen = 26;
 
 	for (j = 0; j < 10; j++)
 	{
-		char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
-		int arrLen;
 		int i;
 
-		arrLen = strlen(alphabets);
 		for (i = 0; i < arrLen; i++)
 		{
-			putchar(alphabets[i]);
+			_putchar(i + a);
 		}
 
-		putchar('\n');
+		_putchar('\n');
 	}
 }
